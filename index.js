0d59070736835035681a821ec2fef88a3057e5aa
Ben Veilleux <147216774+baveilleux@users.noreply.github.com>
2024-08-29 13:29:57 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const serverless = require('serverless-http');
 const winston = require('winston');
 
 const app = express();
